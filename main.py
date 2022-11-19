017df6843aa110984e3d33152102412086e81353
MomoKh <mohammedkhedim123@gmail.com>
2022-11-19 16:24:47 -0500

    Camera with timestamp recognises faces

--- main.py
+++ main.py
@@ -1,15 +1,98 @@
 import numpy as np
 import cv2
+import face_recognition
+from pathlib import Path
+import threading
+import time
+import datetime
 
+
+known = False
 cap = cv2.VideoCapture(0)
+
+momo_img = face_recognition.load_image_file("/home/momo/Documents/Hackathon/facetec_python/imgTest/KnowMohammedKhedim.jpg")
+momo_img_encoding = face_recognition.face_encodings(momo_img)[0]
+
+
+known_face_encodings = [
+    momo_img_encoding
+]
+
+known_face_names = [
+    "Mohammed Khedim"
+]
+
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
 while True:
     ret, frame = cap.read()
+    if ret == True:
 
-    cv2.imshow('frame', frame)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        datet = str(datetime.datetime.now())
+        frame = cv2.putText(frame, datet, (0, 100), font, 0.7,
+                            (0, 255, 255), 2, cv2.LINE_AA)
 
-    if cv2.waitKey(1) == ord('q'):
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+                known = True
+                print( name + "is allowed in!")
+
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.4, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.realease()
+# Release handle to the webcam
+cap.release()
 cv2.destroyAllWindows()
